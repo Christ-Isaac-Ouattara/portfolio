3aaf2742b852cb90a9b9d25b13e609507fde0a35
Christ-Isaac-Ouattara <ouattaraisaacpro@gmail.com>
2025-06-03 04:02:41 +0000

    save

--- data/resumeData.ts
+++ data/resumeData.ts
@@ -16,14 +16,23 @@ interface Experience {
 }
 
 export const projects: Project[] = [
-    {
+  {
+    name: "Site réparation automobile et multiservice",
+    link: "https://ivoire-diesel-plus.vercel.app//",
+    image: "/img/idp.png",
+    range: "Juin 2025",
+    description:
+      "Un site web de réparation automobile et multiservice pour un garage qui propose des services de réparation et de maintenance, de sous traitance minière, de locations d'engins et de vehicule, et travaux publics.",
+    technologies: ["TypeScript", "React", "Next.js"],
+  },
+  {
     name: "Portfolio React",
     link: "https://kriscode.tech/",
     image: "/img/portfolio.png",
     range: "Mai 2025",
     description:
       "Un portfolio moderne construit avec React et Next.js. Comprend des transitions de page fluides et des animations légères mais subtiles inspiré de brittany Chiang.",
-    technologies: ["TypeScript", "React", "Next.js", "MongoDB", "Gsap"],
+    technologies: ["TypeScript", "React", "Next.js", "Motion"],
   },
   {
     name: "Refonte de site web de restauration",
@@ -32,7 +41,7 @@ export const projects: Project[] = [
     range: "Mai 2025",
     description:
       "Un site web de restauration pour un restaurateur qui propose des menus de plats africains en france.",
-    technologies: ["TypeScript", "React", "Next.js", "MongoDB"],
+    technologies: ["TypeScript", "React", "Next.js"],
   },
   {
     name: "Site web e-commerce",
@@ -41,7 +50,7 @@ export const projects: Project[] = [
     range: "Avril 2025 - Present",
     description:
       "Un site web e-commerce qui permet aux utilisateurs de parcourir et d'acheter des produits en ligne.",
-    technologies: ["TypeScript", "React", "Next.js", "MongoDB"],
+    technologies: ["TypeScript", "React", "Next.js", "Resend"],
   },
   {
     name: "Application immobilière",
@@ -50,7 +59,15 @@ export const projects: Project[] = [
     range: "Mars 2025 - Present",
     description:
       "Une application qui permet aux utilisateurs de rechercher des propriétés immobilières, de voir les détails de chaque propriété et de contacter le propriétaire.",
-    technologies: ["TypeScript", "React", "Next.js", "MongoDB"],
+    technologies: [
+      "TypeScript",
+      "React",
+      "Next.js",
+      "MongoDB",
+      "PostgreSQL",
+      "Express.js",
+      "Node.js",
+    ],
   },
   {
     name: "Application de santé en ligne",
@@ -91,43 +108,66 @@ export const experiences: Experience[] = [
         link: "https://www.finteck.aigle.fr/",
         image: "/img/finteck.png",
         range: "Mars 2025 - Present",
-        description: "Mise en place d’une plateforme de transfert sécurisé avec un wallet numérique permettant aussi l’achat de pass internet et crédit d’appel.",
-        technologies: ["TypeScript", "React.js", "Node.js", "Adonis.js", "SQL"]
+        description:
+          "Mise en place d’une plateforme de transfert sécurisé avec un wallet numérique permettant aussi l’achat de pass internet et crédit d’appel.",
+        technologies: ["TypeScript", "React.js", "Node.js", "Adonis.js", "SQL"],
       },
       {
         name: "Demande d’actes d’état civil",
         link: "https://www.finteck.aigle.fr/",
         image: "/img/finteck.png",
         range: "Mars 2025 - Present",
-        description: "Conception du frontend d’un site facilitant la demande et le suivi des actes administratifs auprès des mairies en Côte d’Ivoire.",
-        technologies: ["Javascript", "React.js"]
+        description:
+          "Conception du frontend d’un site facilitant la demande et le suivi des actes administratifs auprès des mairies en Côte d’Ivoire.",
+        technologies: ["Javascript", "React.js"],
       },
       {
         name: "Gestion des comptes bancaires et crédits",
         link: "https://www.finteck.aigle.fr/",
         image: "/img/finteck.png",
         range: "Mars 2025 - Present",
-        description: "Création d'un portail pour l'ouverture de comptes et la demande de crédits avec suivi des dossiers en temps réel.",
-        technologies: ["Javascript", "React.js", "Node.js", "Express.js", "MongoDB"]
+        description:
+          "Création d'un portail pour l'ouverture de comptes et la demande de crédits avec suivi des dossiers en temps réel.",
+        technologies: [
+          "Javascript",
+          "React.js",
+          "Node.js",
+          "Express.js",
+          "MongoDB",
+        ],
       },
-      
+
       {
         name: "Gestion des églises et dons",
         link: "https://www.finteck.aigle.fr/",
-        image: "/img/finteck.png",        
+        image: "/img/finteck.png",
         range: "Mars 2025 - Present",
-        description: "Développement d’un site pour la gestion des dons, événements et annonces d’églises, avec paiement en ligne intégré.",
-        technologies: ["Javascript", "React.js", "Node.js", "Express.js", "MongoDB"]
+        description:
+          "Développement d’un site pour la gestion des dons, événements et annonces d’églises, avec paiement en ligne intégré.",
+        technologies: [
+          "Javascript",
+          "React.js",
+          "Node.js",
+          "Express.js",
+          "MongoDB",
+        ],
       },
       {
         name: "Plateforme E-commerce",
         link: "https://www.finteck.aigle.fr/",
         image: "/img/finteck.png",
         range: "Mars 2025 - Present",
-        description: "Développement d'un site permettant aux commerçants de créer et gérer leur boutique en ligne avec intégration des paiements en ligne locaux.",
-        technologies: ["Javascript", "React.js", "Node.js", "Express.js", "MongoDB"]
+        description:
+          "Développement d'un site permettant aux commerçants de créer et gérer leur boutique en ligne avec intégration des paiements en ligne locaux.",
+        technologies: [
+          "Javascript",
+          "React.js",
+          "Node.js",
+          "Express.js",
+          "MongoDB",
+        ],
       },
-    ]
+    ],
   },
   // Vous pouvez ajouter d'autres expériences ici
   // {
